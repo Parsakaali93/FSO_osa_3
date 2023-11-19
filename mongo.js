c908de67011befbe0d82f9a782099ef6889af852
Parsakaali <maukissamau@gmail.com>
2023-11-19 21:47:51 +0200

    gitignore

--- mongo.js
+++ mongo.js
@@ -6,31 +6,51 @@ if (process.argv.length<3) {
   process.exit(1)
 }
 
+else if (process.argv.length==4) {
+  console.log('Give as arguments: DatabasePassword, NameToInsert, NumberToInsert')
+  process.exit(1)
+}
 const password = process.argv[2]
-
-const url =
-  `mongodb+srv://USERNAMEHERE:${password}@cluster0.63nwogr.mongodb.net/phonebook?retryWrites=true&w=majority`
+const nameVar = process.argv[3]
+const numberVar = process.argv[4]
+const url = `mongodb+srv://misuel:${password}@cluster0.63nwogr.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const contactSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+  name: String,
+  number: String,
 })
 
 const Contact = mongoose.model('Contact', contactSchema)
 
+if(process.argv.length >= 5)
+{
+  const contact = new Contact({
+    name: nameVar,
+    number: numberVar,
+  })
 
-const contact = new Contact({
-  content: 'Tosi iso Kissa',
-  important: true,
-})
+  contact.save().then(result => {
+    console.log(`Added ${nameVar} ${numberVar} to the phonebook`)
+    mongoose.connection.close()
+  })
+}
 
-contact.save().then(result => {
-  console.log('contact saved!')
-  mongoose.connection.close()
-})
+else
+{
+  Contact.find({})
+    .exec()
+    .then((contacts) => {
+      console.log('All contacts in the phonebook:')
+      contacts.forEach(contact => {
+        console.log(`${contact.name}  ${contact.number}`)
+      })
+    })
+    .catch(error => {console.error('Error retrieving contacts:', error)})
+    .finally(() => mongoose.connection.close())
+}
 
 /*Note.find({}).then(result => {
   result.forEach(note => {
